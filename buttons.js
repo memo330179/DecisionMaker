aebe56f38934a08c29aadb4444e910f800d436a8
memo3301791 <guillermoramos330179@gmail.com>
2015-11-23 01:52:03 +0000

    Added buttons and question

---
+++ buttons.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    var max_fields          = 10; //maximum number of fields
+    var wrapper             = $(".input_field_wrap");
+    var add_button          = $(".add_field_button");
+    
+    var x = 2; //we already have two options in there
+    $(add_button).click(function(e){
+        e.preventDefault();
+        if(x < max_fields){
+            x++; //add the number of textboxes
+            $(wrapper).append('<div><input type="text" name="option[]" /><a href="#" class="remove_field">Remove</a></div>');
+        }
+    });
+    
+    $(wrapper).on("click",".remove_field", function(e){
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    });
+});
