ecdbc4ea9ea9c02f51b671ca44848185647cf122
memo3301791 <guillermoramos330179@gmail.com>
2015-11-24 15:32:42 +0000

    Created a header to make the html templated

---
+++ header.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?php echo $title; ?></title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  </head>
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Decision Maker</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  </head>
+
+<?php
+//this is the header for the files
+$title = "Decision Maker"; //title of the page
+include "header.php";
+?>
+
   <body>
     <div class="question">
       <div>Question: <input type="text" name="question"/></div>
@@ -11,11 +12,12 @@
     <form action="decision.php" method="post">
     <div class="input_field_wrap">
       <button class="add_field_button">Add More Options</button>
-      <div>option: <input type="text" name="option[]"></div>
-      <div>option: <input type="text" name="option[]"></div>
+      <div>option: <input type="text" name="option[]" required></div>
+      <div>option: <input type="text" name="option[]" required></div>
       <div class="submit_options"><input type=submit></div>
     </div>
     </form>
     <script type = "text/javascript" src = "buttons.js"></script>
   </body>
+  <!--TODO change this to a footer -->
 </html>
